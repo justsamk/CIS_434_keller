2a4eebac98f86d75852bf5e165cae49ac73c08cf
justsamk <131796320+justsamk@users.noreply.github.com>
2024-03-20 12:00:50 -0400

    Update addition.java

--- addition.java
+++ addition.java
@@ -4,11 +4,16 @@ public class addition {
         int num2 = 9;
 
         int sum = additionMeth(num1, num2);
+        int sub = SubMeth(num1, num2);
 
         System.out.println("Sum: " + sum);
+        System.out.println("Sub: " + sub);
     }
 
     public static int additionMeth(int num1, int num2) {
+        return num1 + num2;
+    }
+     public static int SubMeth(int num1, int num2) {
         return num1 + num2;
     }
 }
