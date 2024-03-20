31c811c3332ed5825909a833a10a94f5811c1a41
Esha <114303966+E5H4@users.noreply.github.com>
2024-03-20 12:01:59 -0400

    Update addition.java

--- addition.java
+++ addition.java
@@ -3,14 +3,14 @@ public class addition {
         int num1 = 6;
         int num2 = 9;
 
-        int sum = additionMeth(num1, num2);
+        int sum = AdditionMeth(num1, num2);
         int sub = SubMeth(num1, num2);
 
         System.out.println("Sum: " + sum);
         System.out.println("Sub: " + sub);
     }
 
-    public static int additionMeth(int num1, int num2) {
+    public static int AdditionMeth(int num1, int num2) {
         return num1 + num2;
     }
      public static int SubMeth(int num1, int num2) {
